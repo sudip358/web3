a0f9c0f8bf1071907f715609ef0a8bb944bb59b9
sudip358 <sudip.tembhurne@vit.edu.in>
2023-09-08 15:00:57 +0530

    fun

---
+++ basics_01/01_MyContract.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: Unlicense
+//dapp university https://www.youtube.com/watch?v=RQzuQb0dfBM
+
+pragma solidity ^0.8.0;
+
+contract MyContract {
+    string public name = "MyContract";
+    uint public version = 1;
+    bool public isPublic = true;
+    int public num = 1;
+    address public addr = 0x0000000000000000000000000000000000000000;
+
+}
+
+
+
